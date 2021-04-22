7008f8a80c0302761959023333cba1f231e22d57
LeoBrasileo <leobraginski@gmail.com>
2021-04-22 17:00:59 -0300

    verificar conjetura

--- TP1.hs
+++ TP1.hs
@@ -20,18 +20,25 @@ esPar :: Int -> Bool
 esPar n = n `mod` 2 == 0
 
 nEsParYMayor :: Int -> Bool
-nEsParYMayor n = esPar n && n > 3
+nEsParYMayor n = esPar n && n > 2
 
-cumpleGoldbachParaEsePrimo :: Int -> Int -> Bool
-cumpleGoldbachParaEsePrimo n k = esPrimo(n-k)
+--k + h = n
+--k y h son los numeros primos y n es el numero par resultado de la suma
+--teniendo en cuenta esa formula n - k o h debe ser primo si o si
+esSumaDePrimosParaEsePrimo :: Int -> Int -> Bool
+esSumaDePrimosParaEsePrimo n k = esPrimo(n-k)
 
-cumpleGoldbach :: Int -> Int -> Bool
-cumpleGoldbach n 1 = False 
-cumpleGoldbach n k | cumpleGoldbachParaEsePrimo n k = True 
-                   | otherwise = cumpleGoldbach n ultPrimo
+esSumaDePrimos :: Int -> Int -> Bool
+esSumaDePrimos n 1 = False 
+esSumaDePrimos n k | esSumaDePrimosParaEsePrimo n k = True 
+                   | otherwise = esSumaDePrimos n ultPrimo
                    where (ultPrimo) = (ultimoPrimoAnterior(k))
 
 satisfaceGoldbach :: Integer -> Bool
-satisfaceGoldbach n | nEsParYMayor nInt == False = False
-                    | otherwise = cumpleGoldbach nInt (ultimoPrimoAnterior(nInt-2))
+satisfaceGoldbach n = nEsParYMayor nInt && esSumaDePrimos nInt (ultimoPrimoAnterior(nInt-2))
                     where (nInt) = fromInteger n
+
+verificarConjeturaHasta :: Integer -> Bool
+verificarConjeturaHasta 4 = True
+verificarConjeturaHasta n | satisfaceGoldbach(n) == False = False
+                          | otherwise = verificarConjeturaHasta (n - 2)

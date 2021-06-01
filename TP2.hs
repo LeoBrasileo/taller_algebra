b48176421a0c0a1bc94216e7e89797142f884d7f
LeoBrasileo <leobraginski@gmail.com>
2021-06-01 13:18:26 -0300

    posiblesJugadas

--- TP2.hs
+++ TP2.hs
@@ -20,3 +20,16 @@ jugar :: Posicion -> Jugada -> Posicion
 jugar p j | valorEnPila - fichasASacar <= 0 = quitarPila p valorEnPila 
           | otherwise = take pilaAModificar p ++ [valorEnPila - fichasASacar] ++ drop (pilaAModificar + 1) p
         where (valorEnPila, pilaAModificar, fichasASacar) = (seleccionarEnLista p pilaAModificar, seleccionarPila j, seleccionarFichas j)
+
+-- pila -> valor en pila -> [Jugada]
+posiblesJugadasPorPila :: Int -> Int -> [Jugada] -> [Jugada]
+posiblesJugadasPorPila p v list | v >= 1 = (p,v) : list ++ posiblesJugadasPorPila p (v-1) list
+                                | otherwise = list
+
+posiblesJugadasConIndice :: Posicion -> Int -> [Jugada] -> [Jugada]
+posiblesJugadasConIndice pos i jugadas | length pos >= i = jugadas ++ posiblesJugadasPorPila i valorEnPila [] ++ posiblesJugadasConIndice pos (i+1) jugadas
+                                       | otherwise = jugadas
+                                    where (valorEnPila) = (seleccionarEnLista pos (i-1))
+
+posiblesJugadas :: Posicion -> [Jugada]
+posiblesJugadas p = posiblesJugadasConIndice p 1 []

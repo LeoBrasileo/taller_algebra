b99402d5928572624ff02b9f9bb3b30859cb3ed2
LeoBrasileo <leobraginski@gmail.com>
2021-06-03 00:08:39 -0300

    probar que una jugada es ganadora

--- TP2.hs
+++ TP2.hs
@@ -5,6 +5,8 @@ type Jugada = (Int,Int)
 
 seleccionarEnLista :: [Int] -> Int -> Int
 seleccionarEnLista xs p = xs!!p
+seleccionarEnJugadas :: [Jugada] -> Int -> Jugada
+seleccionarEnJugadas xs p = xs!!p
 
 seleccionarPila :: Jugada -> Int
 seleccionarPila (a,b) = a - 1
@@ -17,8 +19,8 @@ quitarPila l n | n == head l = tail l
                | otherwise = head l : quitarPila (tail l) n
 
 jugar :: Posicion -> Jugada -> Posicion
-jugar p j | valorEnPila - fichasASacar <= 0 = quitarPila p valorEnPila 
-          | otherwise = take pilaAModificar p ++ [valorEnPila - fichasASacar] ++ drop (pilaAModificar + 1) p
+jugar p j | valorEnPila - fichasASacar <= 0 = quitarPila p valorEnPila --Resta las fichas de la pila
+          | otherwise = take pilaAModificar p ++ [valorEnPila - fichasASacar] ++ drop (pilaAModificar + 1) p --Elimina la pila de la lista
         where (valorEnPila, pilaAModificar, fichasASacar) = (seleccionarEnLista p pilaAModificar, seleccionarPila j, seleccionarFichas j)
 
 -- pila -> valor en pila -> [Jugada]
@@ -33,3 +35,30 @@ posiblesJugadasConIndice pos i jugadas | length pos >= i = jugadas ++ posiblesJu
 
 posiblesJugadas :: Posicion -> [Jugada]
 posiblesJugadas p = posiblesJugadasConIndice p 1 []
+
+--la jugada perdedora por default es [n,n] con n cualquier natural
+--[n,n] siempre es perdedora
+sePuedeLlevarAPosPerdedora :: Posicion -> Bool -> Bool
+sePuedeLlevarAPosPerdedora ps e | length ps > 2 = sePuedeLlevarAPosPerdedora (jugar ps jugadaMasGrande) (not e)
+                                | length ps == 2 && seleccionarEnLista ps 0 == seleccionarEnLista ps 1 = e
+                                | otherwise = sePuedeLlevarAPosPerdedora (llevarAPosPerdedoraFinal ps) (not e)
+                        where (jugadaMasGrande) = (jugadaMasGrandeEnPila ps 1)
+
+--cuando la posicion ya es [n1,n2]
+llevarAPosPerdedoraFinal :: Posicion -> Posicion
+llevarAPosPerdedoraFinal ps | primerValor > segundoValor = jugar ps (1, primerValor - segundoValor)
+                            | otherwise = jugar ps (2, segundoValor - primerValor)
+                          where (primerValor,segundoValor) = (seleccionarEnLista ps 0, seleccionarEnLista ps 1)
+
+--si queda una sola pila podes asegurarte ganar
+--si no hay jugadas posibles es perdedora
+--si hay 2 o mas pilas se puede probar que hay una jugada que garantiza ganar, esto se hace llegando a una posicion perdedora para el otro jugador
+esPosicionGanadora :: Posicion -> Bool  
+esPosicionGanadora ps | length ps == 1 = True 
+                      | null (posiblesJugadas ps) = False
+                      | length ps >= 2 = sePuedeLlevarAPosPerdedora ps False
+                      | otherwise = esPosicionGanadora (jugar ps (1,1))
+
+--Posicion -> pila -> valor mas grande en pila
+jugadaMasGrandeEnPila :: Posicion -> Int -> Jugada
+jugadaMasGrandeEnPila ps p = (p,seleccionarEnLista ps (p-1))

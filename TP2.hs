19c7165e9dde553f3a9f386db475ba2664e5f949
LeoBrasileo <leobraginski@gmail.com>
2021-06-26 20:13:07 -0300

    xor y formula cerrada

--- TP2.hs
+++ TP2.hs
@@ -1,12 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 
 type Posicion = [Int]
 type Jugada = (Int,Int)
-
-esPar :: Int -> Bool
-esPar n = n `mod` 2 == 0
+type Binario = [Int]
 
 jugar :: Posicion -> Jugada -> Posicion
 jugar (p:ps) (i,j) | i == 1 && j >= p = ps
@@ -49,3 +46,35 @@ jugadaGanadoraCount ps (j:js) | not (esPosicionGanadora (jugar ps j)) = 1 + juga
 
 numeroDeJugadasGanadoras :: Posicion -> Int
 numeroDeJugadasGanadoras ps = jugadaGanadoraCount ps (posiblesJugadas ps)
+
+--Funciones recuperatorio
+
+aBinario :: Int -> Binario
+aBinario 0 = []
+aBinario n | n `mod` 2 == 1 = aBinario (n `div` 2) ++ [1]
+           | n `mod` 2 == 0 = aBinario (n `div` 2) ++ [0]
+
+aDecimal :: Binario -> Int
+aDecimal [] = 0
+aDecimal bs = (last bs) + 2 * aDecimal (init bs)
+
+mismaLongitud :: Binario -> Binario -> (Binario, Binario)
+mismaLongitud ns ms | length ns < length ms = mismaLongitud ([0] ++ ns) ms
+                    | length ns > length ms = mismaLongitud ns ([0] ++ ms)
+                    | otherwise = (ns, ms)
+
+xorbin :: Binario -> Binario -> Binario
+xorbin [] [] = []
+xorbin (n:ns) (m:ms) | n /= m = [1] ++ xorbin ns ms
+                     | otherwise = [0] ++ xorbin ns ms
+
+xor :: Int -> Int -> Int
+xor n m = aDecimal (xorbin nF mF)
+        where (nF,mF) = mismaLongitud (aBinario n) (aBinario m)
+
+xorLista :: [Int] -> Int
+xorLista [] = 0
+xorLista (x:xs) = xor x (xorLista xs)
+
+esPosicionGanadoraViaFormulaCerrada :: Posicion -> Bool
+esPosicionGanadoraViaFormulaCerrada ps = xorLista ps /= 0
